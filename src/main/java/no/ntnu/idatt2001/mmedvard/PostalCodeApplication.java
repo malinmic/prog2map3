2ae0484ff5f428e57264c5457f589215d4a87fd7
mmedvard <mmedvard@stud.ntnu.no>
2021-05-12 16:22:48 +0200

    started resolving issue with search

--- src/main/java/no/ntnu/idatt2001/mmedvard/PostalCodeApplication.java
+++ src/main/java/no/ntnu/idatt2001/mmedvard/PostalCodeApplication.java
@@ -3,6 +3,7 @@ import javafx.application.Application;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.collections.transformation.FilteredList;
+import javafx.collections.transformation.SortedList;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.*;
@@ -17,6 +18,8 @@ import no.ntnu.idatt2001.mmedvard.models.PostalCodeRegistry;
 import no.ntnu.idatt2001.mmedvard.controllers.factories.*;
 
 import java.io.*;
+import java.util.ArrayList;
+import java.util.function.Predicate;
 
 public class PostalCodeApplication extends Application{
 
@@ -24,7 +27,7 @@ public class PostalCodeApplication extends Application{
     private MainController mainController;
     private PostalCodeRegistry postalCodeRegistry;
     private ObservableList<PostalCode> postalCodeRegistryWrapper;
-    private ObservableList<PostalCode> data = FXCollections.observableArrayList();
+    private ObservableList<PostalCode> filteredObservableList = FXCollections.observableArrayList();
 
     public static void main(String[] args) {
         launch(args);
@@ -49,28 +52,17 @@ public class PostalCodeApplication extends Application{
         MenuBar mainMenu = MenuBarFactory.create(mainController, postalCodeRegistry,this);
         ToolBar toolBar = ToolBarFactory.create(mainController,postalCodeRegistry,this);
 
-        /*
-        VBox topContainer = new VBox();
-        topContainer.getChildren().add(mainMenu);
-        topContainer.getChildren().add(hBox);
-
-        BorderPane borderPane = new BorderPane();
-        borderPane.setCenter(this.postalCodeTableView);
-        borderPane.setTop(topContainer);
-
-        Scene scene = SceneFactory.create(borderPane);
-        primaryStage.setScene(scene);
-
-        primaryStage.show();
-
-
-         */
 
 
 
 
 
-        FilteredList<PostalCode> filteredList = new FilteredList<>(data, p -> true);
+
+/*
+
+        filteredObservableList.addAll(postalCodeRegistryWrapper);
+
+        FilteredList<PostalCode> filteredList = new FilteredList<>(filteredObservableList, p -> true);
         postalCodeTableView.setItems(filteredList);
 
         ChoiceBox<String> choiceBox = new ChoiceBox<>();
@@ -82,7 +74,7 @@ public class PostalCodeApplication extends Application{
         searchBox.textProperty().addListener((obs, oldValue, newValue) -> {
             switch (choiceBox.getValue()){
                 case "Postal Code":
-                    //filteredList.setPredicate(p -> p.getPostalCode().toLowerCase().contains(newValue.toLowerCase().trim()));
+                    filteredList.setPredicate(p -> p.getPostalCode().toLowerCase().contains(newValue.toLowerCase().trim()));
                     break;
 
                 case "Post Office":
@@ -91,11 +83,11 @@ public class PostalCodeApplication extends Application{
 
 
                 case "Municipality number":
-                    filteredList.setPredicate(p -> p.getMunicipalityNumber().toLowerCase().contains(newValue.toLowerCase().trim()));
+                    //filteredList.setPredicate(p -> p.getMunicipalityNumber().toLowerCase().contains(newValue.toLowerCase().trim()));
                     break;
 
                 case "Municipality name":
-                    filteredList.setPredicate(p -> p.getMunicipalityName().toLowerCase().contains(newValue.toLowerCase().trim()));
+                    //filteredList.setPredicate(p -> p.getMunicipalityName().toLowerCase().contains(newValue.toLowerCase().trim()));
                     break;
 
                 case "Category":
@@ -120,7 +112,16 @@ public class PostalCodeApplication extends Application{
             }
         });
 
-        HBox hBox = new HBox(choiceBox,searchBox);
+
+ */
+
+
+
+        TextField searchBox = new TextField();
+        searchBox.setOnKeyPressed(event -> searchMethod());
+
+
+        HBox hBox = new HBox(searchBox);
         hBox.setAlignment(Pos.TOP_LEFT);
 
 
@@ -136,6 +137,13 @@ public class PostalCodeApplication extends Application{
         primaryStage.setScene(scene);
 
         primaryStage.show();
+
+
+
+
+
+
+
 
 
         /*
@@ -213,7 +221,68 @@ public class PostalCodeApplication extends Application{
 
         //postalCodeTableView.getItems().add(new PostalCode("8800", "Sandnessjøen","10","Alstahaug","G"));
 
+
+
     }
+
+
+    private TableView<PostalCode> searchMethod(){
+
+        filteredObservableList.addAll(postalCodeRegistryWrapper);
+
+        TextField searchBox = new TextField();
+        searchBox.setPromptText("Search");
+
+        FilteredList<PostalCode> filteredList = new FilteredList<>(filteredObservableList, p -> true);
+
+        searchBox.textProperty().addListener((observable, oldValue, newValue) -> {
+            filteredList.setPredicate(post -> {
+                if(newValue == null || newValue.isEmpty()) {
+                    return true;
+                }
+
+                String lowerCase = newValue.toLowerCase();
+
+                if(post.getPostalCode().toLowerCase().contains(lowerCase)) {
+                    return true;
+                }else if(post.getPostOffice().toLowerCase().contains(lowerCase)){
+                    return true;
+                }else{
+                    return false;
+                }
+
+
+
+                /*
+                if(post.getPostalCode().toLowerCase().contains(lowerCase) ||
+                post.getPostOffice().toLowerCase().contains(lowerCase) ||
+                post.getMunicipalityNumber().toLowerCase().contains(lowerCase) ||
+                post.getMunicipalityName().toLowerCase().contains(lowerCase) ||
+                post.getCategory().toLowerCase().contains(lowerCase)) {
+                    return true;
+                }
+                return false;
+
+
+
+                 */
+            });
+
+
+        });
+
+        SortedList<PostalCode> sortedList = new SortedList<>(filteredList);
+
+        sortedList.comparatorProperty().bind(postalCodeTableView.comparatorProperty());
+
+        postalCodeTableView.setItems(sortedList);
+
+        return postalCodeTableView;
+
+
+    }
+
+
 
 
     private void fillTable(){
